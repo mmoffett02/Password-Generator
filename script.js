4ba7597530589ede6189d65287c46e171774aa5e
mmoffett02 <90989673+mmoffett02@users.noreply.github.com>
2022-03-12 18:15:10 -0700

    Add files via upload
    This password generator is designed using javascript to create randomly generated passwords. The website is set so the character length must be between 8 and 128 characters. Window prompts are used to ask the user if they desire lowercase, uppercase, special, or numeric characters included in the password.

---
+++ script.js
@@ -0,0 +1,56 @@
+
+var userChoices= [];
+var characters =[];
+var product= [];
+var numbs = "0123456789".split("");
+var lower= "abcdefghijklmnopqrstuvwxyz".split("");
+var upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var special= "!+-_.@#$%^&*()^/;[]{}<>?~`.".split("");
+
+
+var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  userChoices= [];
+  characters= [];
+  product= [];
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+function generatePassword(){
+  var passwordLength = generatePrompts();
+  for(var i = 0; i < passwordLength; i++){
+    var randomIndex= Math.floor(Math.random() * userChoices.length)
+    var finalPassword = userChoices[randomIndex];
+    product.push(finalPassword);
+  }
+}
+
+function generatePrompts() {
+    console.log ('Hey! You clicked the button')
+     var passwordLength= prompt ("How many characters would you like to include? Must be between 8 and 128."); 
+    
+      if (!passwordLength) {
+        window.alert("You must enter a value between 8 and 128 characters.");
+      }
+     if (isNaN (passwordLength) || passwordLength <8 || passwordLength >128) {
+       passwordLength= window.prompt("You must enter a value etween 8 and 128 characters.");
+     }
+  
+      if (numbs= window.confirm ("Include numeric characters?"));{
+      userChoices = userChoices.concat(numbs);}
+  
+      if (special= window.confirm ("Include special characters?"));{
+      userChoices = userChoices.concat(special);}
+
+      if (upper=window.confirm ("Include uppercase characters?"));{
+      userChoices = userChoices.concat(upper);}
+      
+      if (lower= window.confirm ("Include lowercase characters?"));{
+      userChoices = userChoices.concat(lower);}
+    
+      return password;}
